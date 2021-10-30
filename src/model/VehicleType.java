d72d6fd9415d8b78baea96e55a7c21cee4bccbb3
piyush <piyushmalav85@gmail.com>
2021-10-30 22:14:51 +0530

    remove .idea out, and update vehicleType Enum

--- src/model/VehicleType.java
+++ src/model/VehicleType.java
@@ -1,5 +1,24 @@
+
 package model;
 
 public enum VehicleType {
     CAR , BUS , BIKE
 }
+
+//package model;
+//
+//import static model.VehicleSize.*;
+//
+//public enum VehicleType {
+//    CAR(MEDIUM), BUS(LARGE), BIKE(SMALL);
+//
+//    private final VehicleSize vehicleSize;
+//
+//    VehicleType(VehicleSize vehicleSize) {
+//        this.vehicleSize = vehicleSize;
+//    }
+//
+//    public VehicleSize getVehicleSize() {
+//        return vehicleSize;
+//    }
+//}
